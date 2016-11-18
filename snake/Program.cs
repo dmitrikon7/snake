a620a9bdb675cde816b123b5b3a25be2325e3f3c
dmitrikon7 <dmitrikon7@gmail.com>
2016-11-18 12:42:49 +0300

    setCursorPosition and WrireLine

---
+++ snake/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(20, 5);
+            Console.WriteLine("Привет, привет!");
+            Console.ReadLine();
+        }
+    }
+}
