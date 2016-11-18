74831de63212791fa9c296ea17b1335602228ccd
dmitrikon7 <dmitrikon7@gmail.com>
2016-11-18 13:31:41 +0300

    draw static snake (class Point)

---
+++ snake/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Point
+    {
+        public int x, y;
+        public char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            this.x = _x;
+            this.y = _y;
+            this.sym = _sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(this.x, this.y);
+            Console.WriteLine(this.sym);
+        }
+    }
+}
--- snake/Program.cs
+++ snake/Program.cs
@@ -11,8 +11,28 @@ namespace snake
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
-            Console.SetCursorPosition(20, 5);
-            Console.WriteLine("Привет, привет!");
+
+            Point p1 = new Point(1, 5, '-');
+            Point p2 = new Point(2, 5, '-');
+            Point p3 = new Point(3, 5, '-');
+            Point p4 = new Point(4, 5, '-');
+            Point p5 = new Point(5, 5, '-');
+            Point p6 = new Point(6, 5, '-');
+            Point p7 = new Point(7, 5, '|');
+            Point p8 = new Point(7, 4, '|');
+            Point p9 = new Point(7, 3, '|');
+            Point p10 = new Point(7, 2, '*');
+            p1.Draw();
+            p2.Draw();
+            p3.Draw();
+            p4.Draw();
+            p5.Draw();
+            p6.Draw();
+            p7.Draw();
+            p8.Draw();
+            p9.Draw();
+            p10.Draw();
+
             Console.ReadLine();
         }
     }
